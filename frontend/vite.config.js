8b780631f231863e5ad807a6664332dd8413f7d0
mirfan676 <irfan.karor.mi@gmail.com>
2025-11-02 01:52:05 +0500

    fix netlify issues

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Build output folder
+  },
+  publicDir: 'public', // Ensures files like _redirects are copied to dist
+  server: {
+    port: 5173, // optional: you can change local dev port if needed
+  },
 })
-
