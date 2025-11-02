68d21e1e6b29ae40523e3d543c4eb2bbb367f707
mirfan676 <irfan.karor.mi@gmail.com>
2025-11-02 23:53:16 +0500

    updated city request in backend

--- backend/main.py
+++ backend/main.py
@@ -7,6 +7,7 @@ from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseUpload
 from io import BytesIO
+import requests
 
 app = FastAPI(title="APlus Home Tutors API", version="2.0.0")
 
