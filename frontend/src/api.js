b47f491ab92c37baa29cb3677b3c68acd08c21f9
mirfan676 <irfan.karor.mi@gmail.com>
2025-12-05 11:40:52 +0500

    updated api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,4 +4,10 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL, 
 });
 
+// Named export
+export const fetchJobs = async () => {
+  const response = await api.get("/jobs"); 
+  return response.data;
+};
+
 export default api;
