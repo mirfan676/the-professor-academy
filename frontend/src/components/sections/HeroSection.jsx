5a7b8899c67f4cc6922699d6eb5d08ba4c4112e8
mirfan676 <webservicesaplus@gmail.com>
2025-12-15 00:46:43 +0500

    Updated HeroSection layout for 2 items per row on mobile

--- frontend/src/components/sections/HeroSection.jsx
+++ frontend/src/components/sections/HeroSection.jsx
@@ -181,6 +181,7 @@ export default function HeroSection() {
               "&:hover": {
                 transform: "scale(1.05)",
               },
+              
             }}
           >
             <Box
